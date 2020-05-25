db0bb21f01d39448cd805318c3288591d0cd049b
mistake-git <acbmstk0402@gmail.com>
2020-05-25 20:21:46 +0000

    deviseを導入しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_083423) do
+ActiveRecord::Schema.define(version: 2020_05_25_201447) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
