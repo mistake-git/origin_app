885426ec4cf20c01001525793d6f20f75d46fcfb
mistake-git <acbmstk0402@gmail.com>
2020-07-21 12:19:54 +0000

    ユーザー編集機能を作成しました

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,7 +1,17 @@
 class MypagesController < ApplicationController
-   before_action :set_user,only: [:show]
+   before_action :set_user,only: [:show,:edit,:update]
   
   def show
+       
+  end
+  
+  def edit
+      
+  end
+  
+  def update
+     @user.update(user_params)
+     redirect_to("/mypages/#{@user.id}")
   end
   
   private
@@ -9,5 +19,10 @@ class MypagesController < ApplicationController
   def set_user
       @user = User.find(params[:id])
   end
+  
+  def user_params
+      params.require(:user).permit(:name, :email)
+  end
+    
   
 end
